ef834d9a1fe9e7db23180e4ffba63fd862efa1c3
vkay111 <vkay111@users.noreply.github.com>
2025-04-09 19:44:02 +0100

    fix: remove extra @context wrapper in context definitions

--- vela_did_tool/context_loader.py
+++ vela_did_tool/context_loader.py
@@ -18,65 +18,61 @@ logger = logging.getLogger(__name__)
 CONTEXTS = {
     # W3C Verifiable Credentials Context v1
     "https://www.w3.org/2018/credentials/v1": {
-        "@context": {
-            "@version": 1.1,
-            "@protected": True,
-            "id": "@id",
-            "type": "@type",
-            # Simplified definitions - rely on SECURITY_CONTEXT_V2 for proof details
-            "VerifiableCredential": {"@id": "https://www.w3.org/2018/credentials#VerifiableCredential"},
-            "VerifiablePresentation": {"@id": "https://www.w3.org/2018/credentials#VerifiablePresentation"},
-            "EcdsaSecp256k1Signature2019": {"@id": "https://w3id.org/security#EcdsaSecp256k1Signature2019"},
-            "Ed25519Signature2020": {"@id": "https://w3id.org/security#Ed25519Signature2020"},
-            # Expanded definitions for prefixes
-            "cred": {"@id": "https://www.w3.org/2018/credentials#"},
-            "sec": {"@id": "https://w3id.org/security#"},
-            "xsd": {"@id": "http://www.w3.org/2001/XMLSchema#"},
-            # Property definitions
-            "credentialSchema": {"@id": "cred:credentialSchema", "@type": "@id"},
-            "credentialStatus": {"@id": "cred:credentialStatus", "@type": "@id"},
-            "credentialSubject": {"@id": "cred:credentialSubject", "@type": "@id"},
-            "evidence": {"@id": "cred:evidence", "@type": "@id"},
-            "expirationDate": {"@id": "cred:expirationDate", "@type": "xsd:dateTime"},
-            "holder": {"@id": "cred:holder", "@type": "@id"},
-            "issued": {"@id": "cred:issued", "@type": "xsd:dateTime"},
-            "issuer": {"@id": "cred:issuer", "@type": "@id"},
-            "issuanceDate": {"@id": "cred:issuanceDate", "@type": "xsd:dateTime"},
-            "proof": {"@id": "sec:proof", "@type": "@id", "@container": "@graph"},
-            "refreshService": {"@id": "cred:refreshService", "@type": "@id"},
-            "termsOfUse": {"@id": "cred:termsOfUse", "@type": "@id"},
-            "validFrom": {"@id": "cred:validFrom", "@type": "xsd:dateTime"},
-            "validUntil": {"@id": "cred:validUntil", "@type": "xsd:dateTime"}
-        }
+        "@version": 1.1,
+        "@protected": True,
+        "id": "@id",
+        "type": "@type",
+        # Simplified definitions - rely on SECURITY_CONTEXT_V2 for proof details
+        "VerifiableCredential": {"@id": "https://www.w3.org/2018/credentials#VerifiableCredential"},
+        "VerifiablePresentation": {"@id": "https://www.w3.org/2018/credentials#VerifiablePresentation"},
+        "EcdsaSecp256k1Signature2019": {"@id": "https://w3id.org/security#EcdsaSecp256k1Signature2019"},
+        "Ed25519Signature2020": {"@id": "https://w3id.org/security#Ed25519Signature2020"},
+        # Expanded definitions for prefixes
+        "cred": {"@id": "https://www.w3.org/2018/credentials#"},
+        "sec": {"@id": "https://w3id.org/security#"},
+        "xsd": {"@id": "http://www.w3.org/2001/XMLSchema#"},
+        # Property definitions
+        "credentialSchema": {"@id": "cred:credentialSchema", "@type": "@id"},
+        "credentialStatus": {"@id": "cred:credentialStatus", "@type": "@id"},
+        "credentialSubject": {"@id": "cred:credentialSubject", "@type": "@id"},
+        "evidence": {"@id": "cred:evidence", "@type": "@id"},
+        "expirationDate": {"@id": "cred:expirationDate", "@type": "xsd:dateTime"},
+        "holder": {"@id": "cred:holder", "@type": "@id"},
+        "issued": {"@id": "cred:issued", "@type": "xsd:dateTime"},
+        "issuer": {"@id": "cred:issuer", "@type": "@id"},
+        "issuanceDate": {"@id": "cred:issuanceDate", "@type": "xsd:dateTime"},
+        "proof": {"@id": "sec:proof", "@type": "@id", "@container": "@graph"},
+        "refreshService": {"@id": "cred:refreshService", "@type": "@id"},
+        "termsOfUse": {"@id": "cred:termsOfUse", "@type": "@id"},
+        "validFrom": {"@id": "cred:validFrom", "@type": "xsd:dateTime"},
+        "validUntil": {"@id": "cred:validUntil", "@type": "xsd:dateTime"}
     },
 
     # Security Ed25519 2020 Context
     "https://w3id.org/security/suites/ed25519-2020/v1": {
-        "@context": {
-            "@version": 1.1,
-            "@protected": True,
-            "id": "@id",
-            "type": "@type",
-            # Expanded definitions for prefixes
-            "sec": {"@id": "https://w3id.org/security#"},
-            "xsd": {"@id": "http://www.w3.org/2001/XMLSchema#"},
-            # Define terms used within the proof structure
-            "Ed25519VerificationKey2020": {"@id": "https://w3id.org/security#Ed25519VerificationKey2020"},
-            "Ed25519Signature2020": {"@id": "https://w3id.org/security#Ed25519Signature2020"},
-            "controller": {"@id": "sec:controller", "@type": "@id"},
-            "revoked": {"@id": "sec:revoked", "@type": "xsd:dateTime"},
-            "publicKeyMultibase": {"@id": "sec:publicKeyMultibase"},
-            "challenge": {"@id": "sec:challenge"},
-            "created": {"@id": "http://purl.org/dc/terms/created", "@type": "xsd:dateTime"},
-            "domain": {"@id": "sec:domain"},
-            "expires": {"@id": "sec:expiration", "@type": "xsd:dateTime"},
-            "nonce": {"@id": "sec:nonce"},
-            "proofPurpose": {"@id": "sec:proofPurpose", "@type": "@vocab"},
-            "proofValue": {"@id": "sec:proofValue"},
-            "verificationMethod": {"@id": "sec:verificationMethod", "@type": "@id"},
-            "assertionMethod": {"@id": "sec:assertionMethod", "@type": "@id", "@container": "@set"},
-            "authentication": {"@id": "sec:authenticationMethod", "@type": "@id", "@container": "@set"}
-        }
+        "@version": 1.1,
+        "@protected": True,
+        "id": "@id",
+        "type": "@type",
+        # Expanded definitions for prefixes
+        "sec": {"@id": "https://w3id.org/security#"},
+        "xsd": {"@id": "http://www.w3.org/2001/XMLSchema#"},
+        # Define terms used within the proof structure
+        "Ed25519VerificationKey2020": {"@id": "https://w3id.org/security#Ed25519VerificationKey2020"},
+        "Ed25519Signature2020": {"@id": "https://w3id.org/security#Ed25519Signature2020"},
+        "controller": {"@id": "sec:controller", "@type": "@id"},
+        "revoked": {"@id": "sec:revoked", "@type": "xsd:dateTime"},
+        "publicKeyMultibase": {"@id": "sec:publicKeyMultibase"},
+        "challenge": {"@id": "sec:challenge"},
+        "created": {"@id": "http://purl.org/dc/terms/created", "@type": "xsd:dateTime"},
+        "domain": {"@id": "sec:domain"},
+        "expires": {"@id": "sec:expiration", "@type": "xsd:dateTime"},
+        "nonce": {"@id": "sec:nonce"},
+        "proofPurpose": {"@id": "sec:proofPurpose", "@type": "@vocab"},
+        "proofValue": {"@id": "sec:proofValue"},
+        "verificationMethod": {"@id": "sec:verificationMethod", "@type": "@id"},
+        "assertionMethod": {"@id": "sec:assertionMethod", "@type": "@id", "@container": "@set"},
+        "authentication": {"@id": "sec:authenticationMethod", "@type": "@id", "@container": "@set"}
     }
 }
 
